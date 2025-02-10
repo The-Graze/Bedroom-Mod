11507eed21e81d5567d008a3611645b2f93682dc
Graze <me@graze.cc>
2025-02-10 14:40:46 +0000

    fix?

--- Bedroom Mod/Plugin.cs
+++ Bedroom Mod/Plugin.cs
@@ -1,85 +1,131 @@
 ﻿using System;
+using System.Collections;
 using System.IO;
 using System.Reflection;
 using BepInEx;
 using UnityEngine;
-using Utilla;
+using static OVRPlugin;
 
 namespace Bedroom_Mod
 {
-    /// <summary>
-    /// This is your mod's main class.
-    /// </summary>
-
-    /* This attribute tells Utilla to look for [ModdedGameJoin] and [ModdedGameLeave] */
-    [ModdedGamemode]
-    [BepInDependency("org.legoandmars.gorillatag.utilla", "1.5.0")]
     [BepInPlugin(PluginInfo.GUID, PluginInfo.Name, PluginInfo.Version)]
     public class Plugin : BaseUnityPlugin
     {
-        bool inRoom;
-		public GameObject bedroom;
-		void OnEnable()
+        Transform Stump;
+        GameObject bedroom;
+        Shader fxshd;
+        private void Start()
         {
-            /* Set up your mod here */
-            /* Code here runs at the start and whenever your mod is enabled*/
-
-            HarmonyPatches.ApplyHarmonyPatches();
-            Utilla.Events.GameInitialized += OnGameInitialized;
-			bedroom.SetActive(true);
-			GameObject.Find("custom screen").SetActive(false);
-		}
-
-        void OnDisable()
+            GorillaTagger.OnPlayerSpawned(() => StartCoroutine(LoadAssetBundleCoroutine()));
+        }
+        private IEnumerator LoadAssetBundleCoroutine()
         {
-			/* Undo mod setup here */
-			/* This provides support for toggling mods with ComputerInterface, please implement it :) */
-			/* Code here runs whenever your mod is disabled (including if it disabled on startup)*/
+            using (Stream str = Assembly.GetExecutingAssembly().GetManifestResourceStream("Bedroom_Mod.Assets.bedroom"))
+            {
+                if (str == null)
+                {
+                    yield break;
+                }
 
-			HarmonyPatches.RemoveHarmonyPatches();
-			Utilla.Events.GameInitialized -= OnGameInitialized;
-			bedroom.SetActive(false);
-			GameObject.Find("screen").SetActive(true);
-			GameObject.Find("COC Text").SetActive(true);
-			GameObject.Find("CodeOfConduct").SetActive(true);
-		}
-
-		void OnGameInitialized(object sender, EventArgs e)
-		{
-			/* Code here runs after the game initializes (i.e. GorillaLocomotion.Player.Instance != null) */
-			Stream str = Assembly.GetExecutingAssembly().GetManifestResourceStream("Bedroom_Mod.Assets.bedroom");
-			AssetBundle bundle = AssetBundle.LoadFromStream(str);
-			GameObject sluber = bundle.LoadAsset<GameObject>("bedroom");
-			bedroom = Instantiate(sluber);
-			bedroom.SetActive(true);
-			GameObject.Find("screen").SetActive(false);
-			GameObject.Find("COC Text").SetActive(false);
-			GameObject.Find("CodeOfConduct").SetActive(false);
-		}
-
-		void Update()
-		{	
-
+                AssetBundleCreateRequest bundleRequest = AssetBundle.LoadFromStreamAsync(str);
+                yield return bundleRequest;
+                AssetBundle bundle = bundleRequest.assetBundle;
+                if (bundle == null)
+                {
+                    yield break;
+                }
+                InitializeBedroom(bundle);
+            }
         }
 
-		/* This attribute tells Utilla to call this method when a modded room is joined */
-		[ModdedGamemodeJoin]
-		public void OnJoin(string gamemode)
-		{
-			/* Activate your mod here */
-			/* This code will run regardless of if the mod is enabled*/
+        private void InitializeBedroom(AssetBundle bundle)
+        {
+            bedroom = Instantiate(bundle.LoadAsset<GameObject>("bedroom"));
+            if (bedroom == null)
+            {
+                return;
+            }
+            GameObject.Find("screen")?.SetActive(false);
+            GameObject.Find("COC Text")?.SetActive(false);
+            GameObject.Find("CodeOfConduct")?.SetActive(false);
+            fxshd = bedroom.transform.FindChildRecursive("custom screen (1)").GetComponent<Renderer>().material.shader;
+            bedroom.transform.FindChildRecursive("custom screen (1)").GetComponent<Renderer>().material.mainTexture = LoadEmbeddedTexture("Bedroom_Mod.Assets.pic.png");
+            FixShader(bedroom.transform);
 
-			inRoom = true;
-		}
+            Stump = bedroom.transform.GetChild(5);
+            if (Stump != null)
+            {
+                AdjustStumpTransforms();
+                RemoveUnnecessaryChildren();
+            }
+            bedroom.transform.SetParent(GameObject.Find("Environment Objects/LocalObjects_Prefab/TreeRoom")?.transform);
+            bundle.UnloadAsync(false);
+            Destroy(this);
+        }
+        void AdjustStumpTransforms()
+        {
+            Stump.GetChild(0).localPosition = new Vector3(2.6818f, -9.2773f, 0.3f);
+            Stump.GetChild(0).localRotation = Quaternion.Euler(0, 0, 27.5511f);
 
-		/* This attribute tells Utilla to call this method when a modded room is left */
-		[ModdedGamemodeLeave]
-		public void OnLeave(string gamemode)
-		{
-			/* Deactivate your mod here */
-			/* This code will run regardless of if the mod is enabled*/
+            Stump.GetChild(8).localRotation = Quaternion.Euler(Vector3.zero);
+            Stump.GetChild(8).localPosition = new Vector3(0.7589f, -0.4334f, 1.1661f);
 
-			inRoom = false;
-		}
-	}
+            Stump.GetChild(13).localPosition = new Vector3(5.1252f, -2.8832f, 1.424f);
+            Stump.GetChild(13).localRotation = Quaternion.Euler(2.6766f, 186.1166f, 243.9278f);
+        }
+        void RemoveUnnecessaryChildren()
+        {
+            Stump.GetChild(14)?.gameObject.SetActive(false);
+            Stump.GetChild(5)?.gameObject.SetActive(false);
+            Stump.GetChild(6)?.gameObject.SetActive(false);
+            Stump.GetChild(11)?.gameObject.SetActive(false);
+
+            foreach (Transform t in bedroom.transform)
+            {
+                if (t != Stump)
+                {
+                    Destroy(t.gameObject);
+                }
+            }
+        }
+
+        void FixShader(Transform t)
+        {
+            foreach (Transform tr in t)
+            {
+                FixShader(tr);
+            }
+            Renderer renderer = t.GetComponent<Renderer>();
+            if (renderer != null)
+            {
+                renderer.material.shader = fxshd;
+            }
+        }
+
+        private Texture2D LoadEmbeddedTexture(string resourceName)
+        {
+            Assembly assembly = Assembly.GetExecutingAssembly();
+            using (Stream stream = assembly.GetManifestResourceStream(resourceName))
+            {
+                if (stream == null)
+                {
+                    return null;
+                }
+
+                byte[] imageData = new byte[stream.Length];
+                stream.Read(imageData, 0, imageData.Length);
+
+                Texture2D texture = new Texture2D(2, 2);
+                if (texture.LoadImage(imageData))
+                {
+                    texture.filterMode = FilterMode.Point;
+                    return texture;
+                }
+                else
+                {
+                    return null;
+                }
+            }
+        }
+    }
 }
